cee4427fce6c16c57cfe48a68c411b0bf8ec4ec8
Purnendu <72823479+rajpurnendu@users.noreply.github.com>
2024-01-26 14:25:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  amp: {
+    // Enable hybrid AMP mode
+    target: 'experimental-hybrid',
+  },
+};
 
 module.exports = nextConfig;
